7cc6d6c1efefe367b7b59a60b4d1039cfb006e17
Kaya <kayalion@kayalion.net>
2015-07-02 14:56:05 +0200

    wrong var name

--- src/ride/library/http/HttpFactory.php
+++ src/ride/library/http/HttpFactory.php
@@ -210,7 +210,7 @@ class HttpFactory {
             $body = $this->mergeFiles($body, $_FILES);
         }
 
-        if ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || (isset($_SERVER['SCHEME']) && $_SERVER['SCHEME'] == 'https')) {
+        if ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || (isset($_SERVER['HTTP_SCHEME']) && $_SERVER['HTTP_SCHEME'] == 'https')) {
             $isSecure = true;
         } else {
             $isSecure = false;
